bd789a97647876758c79ac0062497e914f7d2cdf
schulerdesigns <160165853+schulerdesigns@users.noreply.github.com>
2024-08-21 12:07:45 -0400

    changed text

--- Lab215-1/Lab215-1.cpp
+++ Lab215-1/Lab215-1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Greetings, planet. Remember to drink water!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
